af9d0e0f67d24ad45bc06cc8ca2926f84cd0bb5a
rafaelfernandescardoso <rafael.fcardoso@hotmail.com>
2025-01-24 17:11:59 -0300

    feat: enhance CreateProjectDto with Swagger documentation
    - Added ApiProperty decorators to CreateProjectDto for improved Swagger documentation.
    - Included examples and descriptions for the name, description, color, and parentId fields to enhance API usability and clarity.

--- src/projects/projects.dto.ts
+++ src/projects/projects.dto.ts
@@ -1,19 +1,38 @@
 // src/dto/project.dto.ts
 import { IsNotEmpty, IsOptional, IsString, IsUUID, IsBoolean } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateProjectDto {
+  @ApiProperty({
+    example: 'My Business',  // Set the example for the name field
+    description: 'The name of the project',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({
+    example: 'This project is focused on establishing my business.',  // Set the example for the description field
+    description: 'A brief description of the project',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   description?: string;
 
+  @ApiProperty({
+    example: 'blue',  // Set the example for the color field
+    description: 'Color code or name for the project',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   color?: string;
 
+  @ApiProperty({
+    example: null,  // Set the example for the parentId field
+    description: 'The ID of the parent project if this project is a sub-project',
+    required: false,
+  })
   @IsOptional()
   @IsUUID()
   parentId?: string;
